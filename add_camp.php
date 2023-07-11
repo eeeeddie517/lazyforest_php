7d03a0e3f0369a5363e29e75d982f14e22e62fbe
Eddie <eeeeddie517@gmail.com>
2023-07-11 15:30:37 +0800

    20230711

---
+++ add_camp.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Create User</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <div class="container">
+    <div class="py-2">
+      <a class="btn btn-primary" href="camp_info.php">回營地列表</a>
+    </div>
+    <form action="doCreate.php" method="post">
+        <div class="mb-2">
+            <label class="fw-bolder" for="">營地名稱</label>
+            <input type="text" class="form-control" name="camp_name">
+        </div>
+        <div class="mb-2">
+            <label class="fw-bolder" for="">營地地址</label>
+            <input type="text" class="form-control" name="camp_address">
+        </div>
+        <div class="mb-2">
+            <label class="fw-bolder" for="">營主電話</label>
+            <input type="tel" class="form-control" name="camp_phone">
+        </div>
+        <div class="mb-2">
+            <label class="fw-bolder" for="">海拔(公尺)</label>
+            <input type="number" class="form-control" name="camp_altitude">
+        </div>
+        <div class="mb-2">
+            <label class="fw-bolder" for="">營地介紹</label>
+            <br>
+            <textarea name="camp_introduce" id="" cols="30" rows="10"></textarea>
+            <!-- <input type="text" class="form-control" name="name"> -->
+        </div>
+        <div class="mb-2">
+            <label class="fw-bolder" for="">營地注意事項</label>
+            <br>
+            <textarea name="camp_notice" id="" cols="30" rows="10"></textarea>
+            <!-- <input type="text" class="form-control" name="name"> -->
+        </div>
+        
+        <button class="btn btn-primary my-2" type="submit">送出</button>
+    </form>
+  </div>
+</body>
+
+</html>
---
+++ doCreate.php
@@ -0,0 +1,36 @@
+<?php
+
+if (!isset($_POST["camp_name"])) {
+    die("請依正常管道到此頁");
+}
+
+require_once("../db_connect.php");
+
+$camp_name = $_POST["camp_name"];
+$camp_address = $_POST["camp_address"];
+$camp_introduce = $_POST["camp_introduce"];
+$camp_notice = $_POST["camp_notice"];
+$camp_phone = $_POST["camp_phone"];
+$camp_altitude = $_POST["camp_altitude"];
+
+// 代辦事項:
+// JOIN CampHost_List > 抓登入進來的CampHost 的ID > 然後再insert
+
+// echo "$camp_name, $camp_address, $camp_introduce, $camp_notice,  $camp_phone, $camp_altitude";
+
+$sql = "INSERT INTO camp_info (camp_name, camp_address, camp_introduce, camp_notice, camp_phone, camp_altitude, valid) VALUES ('$camp_name', '$camp_address', '$camp_introduce', '$camp_notice', '$camp_phone', '$camp_altitude', 1)";
+
+// echo $sql;
+
+
+if ($conn->query($sql) === TRUE) {
+
+    $latestId = $conn->insert_id;
+    echo "資料表 camp_info 新增資料完成, id 為 $latestId";
+    // header("location: user-list.php");
+
+} else {
+    echo "新增資料錯誤: " . $conn->error;
+}
+
+$conn->close();
